a0f853c0b347b6abf03d355e4fa9efc4aa39cc5a
Lilian Burdianov <lilian.burdianov@gmail.com>
2021-03-29 12:18:33 +0400

    update server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true
+  })
+);
+
+// app.use('/user', require('./routes/userRouter'));
+// app.use('/api', require('./routes/upload'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
